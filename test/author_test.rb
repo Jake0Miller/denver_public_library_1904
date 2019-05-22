197909c94457d2261ff055725f3a345a5db4dcbf
Jake Miller <jake0miller@gmail.com>
2019-05-22 14:15:42 -0600

    Write tests for author class

---
+++ test/author_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
+require './lib/author'
+
+class BookTest < Minitest::Test
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
+
+  def test_attributes
+    assert_equal "Charlotte Bronte", @charlotte_bronte.name
+    assert_equal [], @charlotte_bronte.books
+
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+    assert_equal "Jane Eyre", jane_eyre
+
+    villette = @charlotte_bronte.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villette], @charlotte_bronte.books
+  end
+end
