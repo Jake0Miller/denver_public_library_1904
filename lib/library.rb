e2b1cbef708d8a9623638d0d7e7dba3430aa3c13
Jake Miller <jake0miller@gmail.com>
2019-05-22 14:34:16 -0600

    Write publication timeframe method

--- lib/library.rb
+++ lib/library.rb
@@ -13,4 +13,12 @@ class Library
   def books
     @authors.map { |author| author.books }.flatten
   end
+
+  def publication_time_frame_for(author)
+    start = author.books.min {|a,b| a.publication_year <=> b.publication_year}
+    start = start.publication_year
+    fin = author.books.max {|a,b| a.publication_year <=> b.publication_year}
+    fin = fin.publication_year
+    { start: start, end: fin }
+  end
 end
