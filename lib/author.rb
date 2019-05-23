bc3d0441272d6fdeed9d86590cf5b13b243dbaee
Jake Miller <jake0miller@gmail.com>
2019-05-23 09:36:21 -0600

    Refactor write method

--- lib/author.rb
+++ lib/author.rb
@@ -9,11 +9,10 @@ class Author
   end
 
   def write(title, publication_date)
-    book = Book.new({author_first_name: @first_name,
+    @books << Book.new({author_first_name: @first_name,
                         author_last_name: @last_name,
                         title: title,
                         publication_date: publication_date})
-    @books << book
-    book
+    @books.last
   end
 end
